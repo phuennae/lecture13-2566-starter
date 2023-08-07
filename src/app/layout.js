41ef032d0e31c916eff5976203f2c48f17bca12c
Chayanin <arm.crazyplay@gmail.com>
2023-08-07 11:28:47 +0700

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,28 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Lab13",
+  description: "Next13 Routing",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+          crossOrigin="anonymous"
+        />
+      </head>
+      <body className={inter.className}>
+        <div style={{ maxWidth: "600px" }} className="mx-auto">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+  return (
+    <div className="mt-3">
+      <hr />
+      <p className="text-secondary text-center">Copyright © TOP-10-IMDB</p>
+    </div>
+  );
+};
